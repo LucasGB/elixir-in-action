0c8f661ab50c4702e6eb1ce586127c8cf8df77e7
Lucas Guedes Barboza <lguedes.sowltda@ext.softtek.com>
2025-01-22 23:22:56 +0000

    Adiciona implementação de exercicio todo list concorrente.

---
+++ chapter-06/todo/lib/todo_server.ex
@@ -0,0 +1,33 @@
+defmodule TodoServer do
+  use GenServer
+
+  def start do
+    GenServer.start(TodoServer, nil)
+  end
+
+  def add_entry(todo_server, new_entry) do
+    GenServer.cast(todo_server, {:add_entry, new_entry})
+  end
+
+  def entries(todo_server, date) do
+    GenServer.call(todo_server, {:entries, date})
+  end
+
+
+  def init(_) do
+    {:ok, TodoList.new}
+  end
+
+  def handle_cast({:add_entry, new_entry}, todo_list) do
+    new_state = TodoList.add_entry(todo_list, new_entry)
+    {:noreply, new_state}
+  end
+
+  def handle_call({:entries, date}, _, todo_list) do
+    {
+      :reply,
+      TodoList.entries(todo_list, date),
+      todo_list
+    }
+  end
+end
